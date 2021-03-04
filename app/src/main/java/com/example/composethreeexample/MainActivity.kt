a664350ad25349a86417aa409a615d14eece320c
joselo <chilpatorres@gmail.com>
2021-03-04 16:58:46 -0600

    Add Row Layout Example

--- app/src/main/java/com/example/composethreeexample/MainActivity.kt
+++ app/src/main/java/com/example/composethreeexample/MainActivity.kt
@@ -3,18 +3,12 @@ package com.example.composethreeexample
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -23,13 +17,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composethreeexample.ui.theme.ComposeThreeExampleTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column()
+            RowLayout()
         }
     }
 }
@@ -48,7 +42,7 @@ fun test() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column()
+    RowLayout()
 }
 
 @Composable
@@ -75,6 +69,34 @@ fun Column() {
                 text = "$5.99",
                 style = TextStyle(fontSize = 17.sp, color = Color.Magenta)
             )
+        }
+    }
+}
+
+@Composable
+fun RowLayout() {
+    Column(){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "ITEM 1")
+            Text(text = "ITEM 1")
+        }
+        Spacer(modifier = Modifier.padding(20.dp))
+        Row(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)
+                .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "ITEM 2")
         }
     }
 }
