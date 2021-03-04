939e8f2fa74a691abf2c029bd1aaa73df6e89e14
joselo <chilpatorres@gmail.com>
2021-03-04 14:08:06 -0600

    Add Column

--- app/src/main/java/com/example/composethreeexample/MainActivity.kt
+++ app/src/main/java/com/example/composethreeexample/MainActivity.kt
@@ -3,31 +3,40 @@ package com.example.composethreeexample
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composethreeexample.ui.theme.ComposeThreeExampleTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            test()
+            Column()
         }
     }
 }
 
 @Composable
-fun test(){
-    Column (modifier = Modifier.padding(16.dp)){
+fun test() {
+    Column(modifier = Modifier.padding(16.dp)) {
         Text(text = "Hey look some there")
         Spacer(modifier = Modifier.padding(top = 10.dp))
         Button(onClick = { /*TODO*/ }) {
@@ -39,5 +48,33 @@ fun test(){
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    test()
+    Column()
+}
+
+@Composable
+fun Column() {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Image(
+            painter = painterResource(id = R.drawable.happy_meal_senior_mcd_sweden_cover),
+            contentDescription = "",
+            modifier = Modifier.height(300.dp),
+            contentScale = ContentScale.Crop
+        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Happy Meal",
+                style = TextStyle(fontSize = 26.sp)
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "800 Calories",
+                style = TextStyle(fontSize = 17.sp)
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "$5.99",
+                style = TextStyle(fontSize = 17.sp, color = Color.Magenta)
+            )
+        }
+    }
 }
